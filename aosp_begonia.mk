94db22af3fe4b689728e4f7e086c53a2ee44df96
Azurite <azuritenoah@gmail.com>
2021-04-16 01:35:42 +0900

    begonia: Update redfin fingerprint to Mar 2021
    Signed-off-by: Azurite <azuritenoah@gmail.com>

--- aosp_begonia.mk
+++ aosp_begonia.mk
@@ -37,3 +37,10 @@ PRODUCT_NAME := aosp_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Fingerprint
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_DESCRIPTION := "redfin-user 11 RQ2A.210305.006 7119741 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC=$(BUILD_DESCRIPTION)
